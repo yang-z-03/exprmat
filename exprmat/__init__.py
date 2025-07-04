718364df4e5f70369925665179a9f705719f0158
yang-z. (cc) <xornent@outlook.com>
2025-07-04 19:58:07 +0800

    0.1.28-1
    - automatic configuration loading from .exprmatrc
    - fix display errors, minor update

--- exprmat/__init__.py
+++ exprmat/__init__.py
@@ -8,22 +8,24 @@ import importlib.metadata
 import mudata as mu
 import anndata as ad
 import scanpy as sc
+import pathlib
 
 # core method exports
 from exprmat.utils import setup_styles
 from exprmat.reader.experiment import experiment, load_experiment
 from exprmat.reader.metadata import metadata, load_metadata
-import exprmat.configuration as config
-from exprmat.ansi import warning
+from exprmat.configuration import default as config
+from exprmat.ansi import warning, info
 
 mu.set_options(pull_on_update = False)
 setup_styles()
 
+
 DATABASE_SETUP_MESSAGE = """
 The exprmat package do not come with a included database after version 0.1.25, 
 due to the extensive size of the reference data. You need to setup the database
-and configure it properly with configuration file `exprmat.config` under your
-current working directory.
+and configure it properly with configuration file `.exprmatrc` under your
+user root or current working directory
 
 For more guide on how to setup the database folder, and more information about
 what the database includes, see <https://github.com/yang-z-03/exprmat>
@@ -31,31 +33,44 @@ what the database includes, see <https://github.com/yang-z-03/exprmat>
 
 # load configuration
 PERFORM_DATABASE_CHECK = True
-if os.path.exists('exprmat.config'):
-    with open('exprmat.config', 'r') as f:
-        import json
-        workspace_config = json.load(f)
-        config.default.update(workspace_config)
+default_finders = [
+    'exprmat.config',
+    '.exprmat.config',
+    '.exprmatrc',
+    os.path.join(str(pathlib.Path.home()), 'exprmat.config'),
+    os.path.join(str(pathlib.Path.home()), '.exprmat.config'),
+    os.path.join(str(pathlib.Path.home()), '.exprmatrc')
+]
 
+for finder in default_finders:
+    if os.path.exists(finder):
+        info(f'load configuration from {finder}.')
+        with open(finder, 'r') as f:
+            import json
+            workspace_config = json.load(f)
+            config.update(workspace_config)
 
-# perform database integrity check.
-if PERFORM_DATABASE_CHECK:
-    dbpath = config.default['data']
+def version_db():
+    dbpath = config['data']
     version_file = os.path.join(dbpath, '.bsync', 'current')
-    db_setup = True
-    
-    if not os.path.exists(version_file): db_setup = False
+
+    if not os.path.exists(version_file): 
+        db_ver = None
     else:
         with open(version_file, 'r') as vf:
             db_ver = vf.read().strip()
-        if db_ver != importlib.metadata.version("exprmat"):
-            pkg_ver = importlib.metadata.version("exprmat")
-            warning('the exprmat database version ({db_ver}) is not compatible with package version ({pkg_ver})')
-            db_setup = False
     
-    if not db_setup: 
-        warning('the database is not set.')
+    return db_ver
+
+# perform database integrity check.
+if PERFORM_DATABASE_CHECK:
+    db_ver = version_db()
+    if db_ver is None: 
+        warning('the database is not installed.')
         print(DATABASE_SETUP_MESSAGE)
+    elif db_ver != importlib.metadata.version("exprmat"):
+        warning('the database version do not match the package version')
+        warning(f'db version: {db_ver}  package version: {importlib.metadata.version("exprmat")}')
 
 
 def version(): 
@@ -65,11 +80,13 @@ def version():
     from exprmat.ansi import info, error, format_file_size
 
     ver_string = importlib.metadata.version("exprmat")
+    db_ver = version_db()
+
     MAJOR, MINOR, REVISION = [int(x) for x in ver_string.split('.')]
-    info(f'exprmat {MAJOR}.{MINOR}.{REVISION}')
+    info(f'exprmat {MAJOR}.{MINOR}.{REVISION} / exprmat-db {db_ver if db_ver is not None else "(Not installed)"}')
     info(f'os: {os.name} ({sys.platform})  platform version: {platform.release()}')
     info(f'current working directory: {os.getcwd()}')
-    info(f'current database directory: {config.default["data"]}')
+    info(f'current database directory: {config["data"]} ({db_ver if db_ver is not None else "(Not found)"})')
     memory()
     return (MAJOR, MINOR, REVISION)
 
@@ -86,7 +103,7 @@ def memory():
 
 setwd = os.chdir
 getwd = os.getcwd
-def locate_data(dir): config.default.update_config('data', dir)
+def locate_data(dir): config.update_config('data', dir)
 
 
 __all__ = [
